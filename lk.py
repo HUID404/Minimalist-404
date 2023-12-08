61c492abb24fbe6825f6c00bee0d0352f5bac85c
404 <129621733+HUID404@users.noreply.github.com>
2023-12-08 22:22:49 +0800

    Delete lk.py
    Signed-off-by: 404 <129621733+HUID404@users.noreply.github.com>

--- lk.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-video_url = 'https://example.com/video.mp4'
-
-response = requests.get(video_url)
-if response.status_code == 200:
-    print(response.url)
-else:
-    print('Failed to fetch video URL.')
